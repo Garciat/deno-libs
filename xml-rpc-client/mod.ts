9cf1e23d8c873c30d0f6478b7d9906f0b9cfa090
Gabriel Garcia <gbrlgrct@gmail.com>
2025-02-27 17:48:19 -0500

    xml-rpc-client: export all modules

--- xml-rpc-client/mod.ts
+++ xml-rpc-client/mod.ts
@@ -1,1 +1,11 @@
 export * from "./client.ts";
+
+export * from "./format.ts";
+
+export * from "./json.ts";
+
+export * from "./parse.ts";
+
+export * from "./schema.ts";
+
+export * from "./types.ts";
